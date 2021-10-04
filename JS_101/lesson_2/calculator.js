ee91400b5fc15bdff7f1e6f1fca5a2f0ff133c72
Craig Dunbar <craigdunbar.com>
2021-10-04 15:48:53 -0700

    lesson2 commit

--- JS_101/lesson_2/calculator.js
+++ JS_101/lesson_2/calculator.js
@@ -1,2 +1,77 @@
-// calculator walk through
-// is this committing properly?
+/*
+Calculator
+# -----------------------INSTRUCTIONS--------------------------------------
+# Ask the user for two numbers.
+# Ask the user for the type of operation to
+# perform: add, subtract, multiply or divide.
+# Perform the calculation and display the result.
+# --------------------------PROBLEM----------------------------------------
+
+# Input: numbers, and string
+# Output: log result of operation of string on numbers
+# ---------------------------RULES-----------------------------------------
+# Explicit:
+
+# Implicit:
+
+# --------------------------EXAMPLES---------------------------------------
+
+# ----------------------------ALGO----------------------------------------
+# Ask the user for the first number.
+# Ask the user for the second number.
+# Ask the user fot an operation on the two numbers.
+# Print the result to the terminal
+*/
+const readline = require('readline-sync');
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function inValidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+prompt('Welcome to the Calculator!');
+
+prompt('What is the first number?');
+let number1 = readline.question();
+
+while (inValidNumber(number1)) {
+  prompt("Hmm .. that doesn't look like a valid number.");
+  number1 = readline.question();
+}
+
+prompt('What is the second number?');
+let number2 = readline.question();
+
+while (inValidNumber(number2)) {
+  prompt("Hmm that isn't a valid number");
+  number2 = readline.question();
+}
+
+prompt('What operation would you like to perform? \n1) Add 2) Subtract 3) Multiply 4) Divide');
+let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must chose 1, 2, 3 or 4');
+  operation = readline.question();
+}
+
+let output;
+switch (operation) {
+  case '1':
+    output = Number(number1) + Number(number2);
+    break;
+  case '2':
+    output = Number(number1) - Number(number2);
+    break;
+  case '3':
+    output = Number(number1) * Number(number2);
+    break;
+  case '4':
+    output = Number(number1) / Number(number2);
+    break;
+}
+
+prompt(`The result is ${output}`);
---
+++ JS_101/lesson_2/pseudocode.js
@@ -0,0 +1,24 @@
+// a function that returns the sum of two numbers 
+// Get the first number from the user
+// Get the second number from the user
+// Set a result variable equal to the first number plus the second number
+// Print the result variable to the console.
+
+// a function that takes an array of strings, and returns a 
+// string that is all those strings concatenated
+// - set a new empty string
+// - while not at the last array element 
+//   iterate through the given array adding each string element 
+//   to the new string.
+// once the last element is added Print the new string.
+
+// a function that takes an array of integers, and
+// returns a new array of every other element
+// - set a new empty array
+// - set up a counter at zero 
+// - while not at the last element of the array 
+//   iterate throught the array starting at the index of the counter
+//   add the element to the new array
+// - skip the next element and move to the element at index 2
+//   by incrementing the counter by 2
+// - return the new array
