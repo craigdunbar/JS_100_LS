c0db48b56cbc568bbcd1e8bdab0a21cfee07ad83
Craig Dunbar <craigdunbar@hotmail.com>
2021-10-06 22:43:07 +0000

    easy1 commit

--- JS_101/small_problems/easy_1/ex_7.js
+++ JS_101/small_problems/easy_1/ex_7.js
@@ -1,12 +1,10 @@
 // short long short
 // take two strings as parameters to a function
-// dtermine which string is shorter
-// concatenate the strins with short string, long string, and short string
+// determine which string is shorter
+// concatenate the strings with short string, long string, and short string
 // return the concatenated string
 
 function shortLongShort(str1, str2) {
-    // let length1 = str1.length;
-    // let length2 = str2.length;
     
     if (str1.length < str2.length) {
         return (str1 + str2 + str1);
