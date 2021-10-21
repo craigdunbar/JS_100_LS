b5f28f001d570f8a10e2c5e6bcc2418b97b6afc3
Craig Dunbar <craigdunbar.com>
2021-10-21 15:21:46 -0700

    mortgage_calc commit

--- JS_101/lesson_2/mortgage_calculator.js
+++ JS_101/lesson_2/mortgage_calculator.js
@@ -5,7 +5,7 @@
 # the APR and the loan duration.
 # from this calculate the monthly payment.
 # --------------------------PROBLEM--------------------------
-# Input: strings 
+# Input: strings
 # Output: number with 2 decimal places
 # ---------------------------RULES---------------------------
 # Explicit: let m = p * (j / (1 - Math.pow((1 + j), (-n))));
@@ -13,38 +13,123 @@
             p = loan amount
             j = monthly interest rate
             n = loan duration in months
-# Implicit: 
+# Implicit:
 
 # --------------------------EXAMPLES-------------------------
 
 # ----------------------------ALGO---------------------------
-# require readline-sync 
+# require readline-sync
 # ask the user for the loan amount in dollars
 # ask the user for the APR
 # aks the user for the loan duration in months
 # calculcate the monthly payment using the formula
 # print the monthly payment
 */
+
+const MESSAGES = require('./mortgage_calculator_messages.json');
+let rlSync = require('readline-sync');
+
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+function emptyLine() {
+  console.log('');
+}
+
 function invalidNumber(number) {
-  return number.trimStart() === '' || 
+  return number.trimStart() === '' ||
   Number.isNaN(Number(number)) ||
   number === '0' ||
+  number < 1 ||
   number.startsWith('-');
 }
 
-let rlSync = require('readline-sync');
-let loanAmount = rlSync.question(prompt("Enter the loan amount:"));
-while (invalidNumber(loanAmount)) {
-  prompt("This is not a valid number");
-  loanAmount = rlSync.question(prompt("Enter the loan amount:"));
+function invalidPercentage(percent) {
+  return percent > 100 ||
+  percent <= 0 ||
+  percent.trimStart() === '' ||
+  percent.startsWith('-') ||
+  Number.isNaN(Number(percent));
 }
 
-let APR = rlSync.question(prompt("Enter the Annual Percentage Rate (APR):"));
-let loanDuration = rlSync.question(prompt("Enter the loan duration in months:"));
-let monthlyRate = ((APR / 100) / 12);
-let monthlyPayment = loanAmount * (monthlyRate / (1 - Math.pow((1 + monthlyRate), (-loanDuration))));
-console.log(`The monthly payment is $${monthlyPayment.toFixed(2)}`);
+function getLoan() {
+  prompt(MESSAGES['loanAmount']);
+  let loanAmount = rlSync.question();
+  while (invalidNumber(loanAmount)) {
+    prompt(MESSAGES['invalidNumber']);
+    loanAmount = rlSync.question(prompt(MESSAGES['loanAmount']));
+  }
+  return Number(loanAmount);
+}
+
+function getAPR() {
+  prompt(MESSAGES['apr']);
+  let APR = rlSync.question();
+  while (invalidPercentage(APR)) {
+    prompt(MESSAGES['invalidPercent']);
+    APR = rlSync.question(prompt(MESSAGES['apr']));
+  }
+  return Number(APR);
+}
+
+function getDuration() {
+  prompt(MESSAGES['loanDuration']);
+  let loanDuration = rlSync.question();
+  while (invalidNumber(loanDuration)) {
+    prompt(MESSAGES['invalidNumber']);
+    loanDuration = rlSync.question(prompt(MESSAGES['loanDuration']));
+  }
+  return Number(loanDuration);
+}
+
+function calcMonthlyPayment(APR, loanAmount, loanDuration) {
+  let monthlyRate = ((APR / 100) / 12);
+  let monthlyPayment = loanAmount * (monthlyRate /
+    (1 - Math.pow((1 + monthlyRate), (-loanDuration))));
+  return monthlyPayment;
+}
+
+function displayPayment(monthlyPayment) {
+  console.log(`The monthly payment is $${monthlyPayment.toFixed(2)}`);
+}
+
+function askToPlayAgain() {
+  prompt(MESSAGES['anotherCalc']);
+  let again = rlSync.question();
+  while (again !== 'y' && again !== 'n') {
+    prompt(MESSAGES['invalidAnswer']);
+    again = rlSync.question(prompt(MESSAGES['anotherCalc']));
+  }
+  return again;
+}
+console.clear();
+
+
+console.clear();
+prompt(MESSAGES['welcome']);
+emptyLine();
+
+while (true) {
+
+  let loanAmount = getLoan();
+  emptyLine();
+
+  let APR = getAPR();
+  emptyLine();
+
+  let loanDuration = getDuration();
+  emptyLine();
+
+  let monthlyPayment = calcMonthlyPayment(APR, loanAmount, loanDuration);
+
+  displayPayment(monthlyPayment);
+  emptyLine();
+
+  let again = askToPlayAgain();
+  if (again === 'n') {
+    prompt(MESSAGES['goodbye']);
+    break;
+  }
+  console.clear();
+}
