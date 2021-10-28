6cc2b163bbf29dac8b30acf82d839ba5a1baaff2
Craig Dunbar <craigdunbar.com>
2021-10-28 06:26:02 -0700

    easy4

--- JS_101/small_problems/easy_4/ex_8.js
+++ JS_101/small_problems/easy_4/ex_8.js
@@ -1,5 +1,5 @@
 // Letter count (part 1)
-// funciton takes a string consisting of zero or more space seperated words 
+// funciton takes a string consisting of zero or more space seperated words
 // and returns an object that shows the number of words of different sizes.
 // note words consist of any non-space characters.
 
@@ -16,20 +16,21 @@
 // return the object
 
 function wordSizes(string) {
-    let arr = string.split(' ');
-    let obj = {}
-    for (count = 0; count < arr.length; count ++) {
-        let wordSize = (arr[count].length);
-        if(obj.keys !== wordSize) {
-            obj[wordSize] = 1
-        } else {
-            obj[wordSize] += 1;
-        }
-        }
-        return obj
+  let arr = string.split(' ');
+  let obj = {};
+  for (let count = 0; count < arr.length; count++) {
+    let wordSize = arr[count].length;
+    // if(!obj.hasOwnProperty(wordSize)) {
+    if (!obj[wordSize]) {
+      obj[wordSize] = 1;
+    } else {
+      obj[wordSize] += 1;
+    }
+  }
+  return obj;
 }
 
 console.log(wordSizes('Four score and seven.'));                       // { "3": 1, "4": 1, "5": 1, "6": 1 }
 console.log(wordSizes('Hey diddle diddle, the cat and the fiddle!'));  // { "3": 5, "6": 1, "7": 2 }
 console.log(wordSizes("What's up doc?"));                              // { "2": 1, "4": 1, "6": 1 }
-console.log(wordSizes(''))        // {};
+console.log(wordSizes(''));        // {};
