0a134cb3bff9ad47227db719c02dff81f6b8513f
Craig Dunbar <craigdunbar.com>
2021-11-10 06:58:24 -0800

    easy2

---
+++ JS_101/lesson_3/easy_2.js
@@ -0,0 +1,17 @@
+/*
+Question 1
+Given a string return a new string that replaces every
+occurence of the word 'important' with 'urgent':
+
+let advice = "Few things in life are as important as house training your pet dinosaur.";
+console.log(advice.replace('important', 'urgent'));
+
+Further exploration:
+Remove multiple occurrences of the same word
+advice = advice.replace(/important/g, 'urgent');
+advice = advice.replaceAll('important', 'urgent'); // doesn't work?
+console.log(advice);
+
+Question 2
+
+*/
