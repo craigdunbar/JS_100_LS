4e373015c13579b18b473659a23e7f8a2a898630
Craig Dunbar <craigdunbar.com>
2021-11-11 14:57:40 -0800

    easy2

--- JS_101/lesson_3/easy_2.js
+++ JS_101/lesson_3/easy_2.js
@@ -34,7 +34,7 @@ let sortedArr = numbers.slice().sort((num1, num2) => num2 - num1);
 console.log(sortedArr);
 console.log(numbers);
 
-// worling through the sort..
+// working through the sort for my own understanding..
 var numbers = [4, 2, 5, 1, 3];
 numbers.sort(function(a, b) {
   console.log(`a = ${a} b = ${b}`);
@@ -58,4 +58,73 @@ numbers = [4, 2, 5, 1, 3];
 1 2 4 5 3  (checked 1 & 2)
 1 2 3 4 5  (checked 3 & 4)
 1 2 3 4 5  (checked 2 & 3)
+
+// using forEach
+let numbers = [1, 2, 3, 4, 5];
+let revArr = []
+
+numbers.forEach((number) => {
+  revArr.unshift(number);
+})
+console.log(revArr);
+
+Question 3
+Given a number and an array, determine if whether the number 
+is included in the array
+
+let numbers = [1, 2, 3, 4, 5, 15, 16, 17, 95, 96, 99];
+let number1 = 8;  // false
+let number2 = 95; // true
+console.log(numbers.includes(number1));
+console.log(numbers.includes(number2));
+
+Question 4
+Starting with the string: 
+let famousWords = "seven years ago...";
+
+show two different ways to put the expected "Four score and" in frony of it.
+let famousWords = "seven years ago...";
+console.log(`Four score and ${famousWords}`);
+console.log("Four score and " + famousWords);
+console.log("Four score and".concat(' ',famousWords));
+
+Question 5
+Given the array of numbers [1, 2, 3, 4, 5], mutate the array 
+by removing the number at index 2, so the array becomes
+[1, 2, 4, 5]
+
+let arr = [1, 2, 3, 4, 5]
+arr.splice(2, 1);
+console.log(arr);
+
+Question 6
+Suppose we build an array like this:
+let flintstones = ["Fred", "Wilma"];
+flintstones.push(["Barney", "Betty"]);
+flintstones.push(["Bambam", "Pebbles"]);
+
+THis will create a nested array:
+["Fred", "Wilma", ["Barney", "Betty"], ["Bambam", "Pebbles"]];
+
+Create an array that contains the above values, 
+but in an un-nested format:
+
+I used array.flat()
+console.log(flintstones.flat());
+
+Using reduce
+
+flintstones = flinstones.reduce((accum, el) => {
+  return accum.concat(element);
+}, []);
+
+Using forEach
+
+let newArr = []
+flintstones.forEach(el => {
+  newArr = newArr.concat(el);
+})
+
+Question 7
+
 */
