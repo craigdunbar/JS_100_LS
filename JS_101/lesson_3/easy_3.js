ee35fcf5e0f6fc98446e481a7498420359cfab38
Craig Dunbar <craigdunbar.com>
2021-11-12 06:41:46 -0800

    easy3

---
+++ JS_101/lesson_3/easy_3.js
@@ -0,0 +1,38 @@
+/*
+Question 1
+Write three different ways to remove all the elements 
+from the following array:
+let numbers = [1, 2, 3, 4];
+
+numbers.splice();
+numbers = [];
+while (numbers.length) {
+  numbers.pop()
+}
+
+Question 2
+What will the folllowing code output?
+will be converted to strings and then concatenated.
+console.log([1, 2, 3] + [4, 5]); // 1, 2, 34, 5
+
+
+Question 3
+What will the following code output?
+let str1 = "hello there";
+let str2 = str1;
+str2 = "goodbye!";
+console.log(str1); // 'hello there'
+
+=====> Question 4 - review
+
+What will then following code output?
+let arr1 = [{ first: "value1" }, { second: "value2" }, 3, 4, 5];
+let arr2 = arr1.slice();
+arr2[0].first = 42;
+console.log(arr1); // [{ first: 42 }, { second: "value2" }, 3, 4, 5]
+
+Question 5
+
+
+*/
+
