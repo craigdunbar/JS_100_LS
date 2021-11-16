7e6a90c2fd581901e12d888a94f86518cc42695f
Craig Dunbar <craigdunbar.com>
2021-11-16 15:31:16 -0800

    lists

--- JS_101/small_problems/list_processing/ex_4.js
+++ JS_101/small_problems/list_processing/ex_4.js
@@ -16,15 +16,26 @@ initalise an empty array
 iterate over the string returning the substrings 
 start with the fist letter and end woth the full string
 */
+// function leadingSubstrings(str) {
+//   let arr = [];
+//   for (let idx = 0; idx < str.length; idx++) {
+//     arr.push(str.slice(0, str.length - idx));
+//   }
+//   return(arr.reverse());
+// }
+
+// further exploration without for loop
+
+// function leadingSubstrings(str) {
+//   newArr = str.split('').map((char, index) => {
+//     return str.slice(0,index + 1);
+//   });
+//   return newArr;
+// }
+
+// refactored
 function leadingSubstrings(str) {
-    let arr = [];
-    // let count = 0
-    for (let idx = 0; idx < str.length; idx++) {
-        arr.push(str.slice(0, str.length - idx));
-        // arr.push(str.slice(count, count + 2));
-        // arr.push(str.slice(count, count + 3));
-    }
-    return(arr.reverse());
+return str.split('').map((char, index) => str.slice(0,index + 1));
 }
 
 console.log(leadingSubstrings('abc')); //===["a", "ab", "abc"]);
