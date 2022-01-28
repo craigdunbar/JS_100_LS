7415705c4cb112f9de357069ae43295f6e33a405
Craig Dunbar <craigdunbar@hotmail.com>
2022-01-28 22:04:46 +0000

    strings and text

---
+++ JS_101/small_problems/string_and_text/ex_10.js
@@ -0,0 +1,39 @@
+/*
+# -----------------------INSTRUCTIONS------------------------
+
+# Search word 2
+Write a function that takes a word and some text as arguments
+and returns the text with every instance of the word highlighted,
+by enclosing the word in ** on each side and making the word 
+uppercase ie **WORD**
+
+# --------------------------PROBLEM--------------------------
+
+# Input: string and sentence
+# Output: sentence
+# ---------------------------RULES---------------------------
+# Explicit: change every instance of word string to **word**
+
+# Implicit: 
+
+# --------------------------EXAMPLES-------------------------
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Sed quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur blasedbla?';
+
+searchWord('sed', text);
+// returns
+// "**SED** ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, **SED** quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, **SED** quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? **SED** quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur blasedbla?"
+# ----------------------------ALGO---------------------------
+# we cna use the same idea as previous problem
+but instead of counting the instances we change the instances to the **WORD** style
+*/
+function searchWord(str, sentence) {
+    let newArr = []
+sentence.split(' ').forEach(word => {
+  word.toLowerCase() === str ? newArr.push(`**${str.toUpperCase()}**`) : newArr.push(word);
+});
+return newArr.join(' ');
+}
+
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Sed quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur blasedbla?';
+
+console.log(searchWord('sed', text) === "**SED** ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, **SED** quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, **SED** quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? **SED** quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur blasedbla?");
--- JS_101/small_problems/string_and_text/ex_9.js
+++ JS_101/small_problems/string_and_text/ex_9.js
@@ -31,19 +31,19 @@ iterate through the array of words
   counter by one.
 return the counter
 */
-// function searchWord(str, sentence) {
-// let counter = 0;
-// sentence.split(' ').forEach(word => {
-//   word.toLowerCase() === str ? counter += 1 : counter;
-// });
-// return counter;
-// }
+function searchWord(str, sentence) {
+let counter = 0;
+sentence.split(' ').forEach(word => {
+  word.toLowerCase() === str ? counter += 1 : counter;
+});
+return counter;
+}
 const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
 
 // LS solution using regex:
-function searchWord(word, text) {
-  const regex = new RegExp(word, 'gi');
-  const matches = text.match(regex);
-  return matches ? matches.length : 0;
-}
+// function searchWord(word, text) {
+//   const regex = new RegExp(word, 'gi');
+//   const matches = text.match(regex);
+//   return matches ? matches.length : 0;
+// }
 console.log(searchWord('sed', text));      // 3
