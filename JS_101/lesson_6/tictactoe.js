2efd77f27544c065ba7d745f84df147ba64179a5
Craig Dunbar <craigdunbar@hotmail.com>
2022-02-02 22:22:00 +0000

    ttt update

--- JS_101/lesson_6/tictactoe.js
+++ JS_101/lesson_6/tictactoe.js
@@ -4,6 +4,7 @@ const HUMAN_MARKER = 'X';
 const COMPUTER_MARKER = 'O';
 
 function displayBoard() {
+  console.clear();
   console.log('');
   console.log('     |     |');
   console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}  `);
@@ -56,10 +57,53 @@ function computerChoosesSquare(board) {
 
   board[square] = COMPUTER_MARKER;
 }
+function boardFull(board) {
+  return emptySquares(board).length === 0;
+}
+
+function someoneWon(board) {
+  return !!detectWinner(board);
+}
+
+function detectWinner(board) {
+  let winningLines = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], // coumns
+    [1, 5, 9], [3, 5, 7]            // diagonal
+  ];
+  
+  for (let line = 0; line < winningLines.length; line ++) {
+    let [sq1, sq2, sq3] = winningLines[line];
+    
+    if (
+      board[sq1] === HUMAN_MARKER &&
+      board[sq2] === HUMAN_MARKER &&
+      board[sq3] === HUMAN_MARKER
+  ) {
+    return 'Player';
+  } else if (
+     board[sq1] === COMPUTER_MARKER &&
+    board[sq2] === COMPUTER_MARKER &&
+      board[sq3] === COMPUTER_MARKER
+      ) {
+        return 'Computer';
+      }
+  }
+  return null;
+}
+
 let board = initalizeBoard();
 displayBoard(board);
 
+while(true) {
 playerChoosesSquare(board);
 computerChoosesSquare(board);
-
 displayBoard(board);
+
+if (someoneWon(board) || boardFull(board)) break;
+}
+if (someoneWon(board)) {
+  prompt(`${detectWinner(board)} won!`);
+} else {
+  prompt("It'a a tie!");
+}
