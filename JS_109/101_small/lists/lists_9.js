0c2e086e8b7eb1d009af3004ee16bebc02b9dd5e
Craig Dunbar <craigdunbar@hotmail.com>
2022-06-14 20:44:49 +0000

    lists

---
+++ JS_109/101_small/lists/lists_9.js
@@ -0,0 +1,65 @@
+/*
+# -----------------------INSTRUCTIONS------------------------
+Inventory Item Transactions
+Write a function that takes two arguments, an inventory item 
+ID and a list of transactions, and returns an array containing 
+only the transactions for the specified inventory item.
+
+# --------------------------PROBLEM--------------------------
+
+# Input: object value and an array
+# Output: array
+# ---------------------------RULES---------------------------
+# Explicit: 
+
+# Implicit: 
+
+# --------------------------EXAMPLES-------------------------
+let transactions = [ { id: 101, movement: 'in',  quantity:  5 },
+                     { id: 105, movement: 'in',  quantity: 10 },
+                     { id: 102, movement: 'out', quantity: 17 },
+                     { id: 101, movement: 'in',  quantity: 12 },
+                     { id: 103, movement: 'out', quantity: 20 },
+                     { id: 102, movement: 'out', quantity: 15 },
+                     { id: 105, movement: 'in',  quantity: 25 },
+                     { id: 101, movement: 'out', quantity: 18 },
+                     { id: 102, movement: 'in',  quantity: 22 },
+                     { id: 103, movement: 'out', quantity: 15 }, ];
+
+transactionsFor(101, transactions);
+// returns
+// [ { id: 101, movement: "in",  quantity:  5 },
+//   { id: 101, movement: "in",  quantity: 12 },
+//   { id: 101, movement: "out", quantity: 18 }, ]
+# ----------------------------ALGO---------------------------
+# since transactions is an array with object elements we can use
+filter to return the requested entries
+*/
+
+let transactions = [ { id: 101, movement: 'in',  quantity:  5 },
+                     { id: 105, movement: 'in',  quantity: 10 },
+                     { id: 102, movement: 'out', quantity: 17 },
+                     { id: 101, movement: 'in',  quantity: 12 },
+                     { id: 103, movement: 'out', quantity: 20 },
+                     { id: 102, movement: 'out', quantity: 15 },
+                     { id: 105, movement: 'in',  quantity: 25 },
+                     { id: 101, movement: 'out', quantity: 18 },
+                     { id: 102, movement: 'in',  quantity: 22 },
+                     { id: 103, movement: 'out', quantity: 15 }, ];
+
+// using forEach..
+// transactions.forEach(obj => {
+//     if (Object.values(obj).includes(101) === true) {
+//         console.log(obj);
+//         return obj
+//     }
+// });
+
+function transactionsFor(itemID, array) {
+    return array.filter(transaction => transaction['id'] === 101)
+}
+console.log(transactionsFor(101, transactions));
+// returns
+// [ { id: 101, movement: "in",  quantity:  5 },
+//   { id: 101, movement: "in",  quantity: 12 },
+//   { id: 101, movement: "out", quantity: 18 }, ]
