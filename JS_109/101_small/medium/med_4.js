30bb0c015c1a88a1be46261f5336df006c0f59a6
Craig Dunbar <craigdunbar@hotmail.com>
2022-06-21 19:19:48 +0000

    medium

--- JS_109/101_small/medium/med_4.js
+++ JS_109/101_small/medium/med_4.js
@@ -69,38 +69,88 @@ to the stack array
 if the string is MULT pop a value from the array and multiply the register by this 
 value and store in the result in the register.
 */
-function minilang(string) {
-    let register = 0;
-    let stack = [];
-    let programArr = string.split(' ');
+// function minilang(string) {
+//     let register = 0;
+//     let stack = [];
+//     let programArr = string.split(' ');
  
-    for (let idx = 0; idx < programArr.length; idx++) {
-        let command = programArr[idx];
-            if (!isNaN(Number(command))) {
-              register = Number(command);  
-            } else if (command === 'PUSH') {
-                stack.push(register);
-            } else if (command === 'MULT') {
-                register = (register * stack.pop());
-            } else if (command === 'PRINT') {
-                console.log(register)
-            } else if (command === 'ADD') {
-                register = (register + stack.pop());
-            } else if (command === 'POP') {
-                register = stack.pop();
-            }  else if (command === 'DIV') {
-                register = Math.floor(register / stack.pop());
-            } else if (command === 'REMAINDER') {
-                register = Math.floor(register % stack.pop());
-            } else if (command === 'SUB') {
-                register = (register - stack.pop());
-            }
-    }
-    return (register)
-}
+//     for (let idx = 0; idx < programArr.length; idx++) {
+//         let command = programArr[idx];
+//             if (!isNaN(Number(command))) {
+//               register = Number(command);  
+//             } else if (command === 'PUSH') {
+//                 stack.push(register);
+//             } else if (command === 'MULT') {
+//                 register = (register * stack.pop());
+//             } else if (command === 'PRINT') {
+//                 console.log(register)
+//             } else if (command === 'ADD') {
+//                 register = (register + stack.pop());
+//             } else if (command === 'POP') {
+//                 register = stack.pop();
+//             }  else if (command === 'DIV') {
+//                 register = Math.floor(register / stack.pop());
+//             } else if (command === 'REMAINDER') {
+//                 register = Math.floor(register % stack.pop());
+//             } else if (command === 'SUB') {
+//                 register = (register - stack.pop());
+//             }
+//     }
+//     return (register)
+// }
 
 // refactor with a switch/ case statement
+function minilang(string) {
+  let register = 0;
+  let stack = [];
+  let programArr = string.split(' ');
 
+ for (let idx = 0; idx < programArr.length; idx++) {
+    let command = programArr[idx];
+
+    switch (command) {
+
+    case 'PUSH':
+      stack.push(register);
+      break;
+    case 'MULT':
+      register = (register * stack.pop());
+      break;
+    case 'PRINT':
+      console.log(register);
+      break;
+    case 'ADD':
+      register = (register + stack.pop());
+      break;
+    case 'POP':
+      register = stack.pop();
+      break;
+    case 'DIV':
+      register = Math.floor(register / stack.pop());
+      break;
+    case 'REMAINDER':
+      register = Math.floor(register % stack.pop());
+      break;
+    case 'SUB':
+      register = (register - stack.pop());
+      break;
+    default:
+      register = Number(command);
+    }
+ }
+return register;
+}
+
+// Further Exploration
+// Refactor the minilang function to include some error handling. 
+// In particular, the function should detect and report empty stack conditions 
+// (trying to use a value from the stack when there are no values), 
+// and invalid tokens in the program. Ideally, the function should return 
+// an error message if an error occurs, or undefined if the program runs successfully.
+// couls use a RegExp test to check for non - alphanumeric characters ie
+
+// (/[^a-z0-9]/gi).test(command) ? console.log("error") : register = Number(command);
+// THis will display an error message but not break out of the for loop
 
 // minilang('PRINT');
 // // 0
@@ -116,7 +166,7 @@ function minilang(string) {
 // minilang('5 PUSH POP PRINT');
 // // 5
 
-// minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT');
+minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT');
 // // 5
 // // 10
 // // 4
@@ -131,5 +181,5 @@ function minilang(string) {
 // minilang('-3 PUSH 5 SUB PRINT');
 // // 8
 
-minilang('6 PUSH');
+// minilang('6 PUSH');
 // // (nothing is printed because the `program` argument has no `PRINT` commands)
