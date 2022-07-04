4e866f734957cd79310abe98da2e9a7d58167fa9
Craig Dunbar <craigdunbar@hotmail.com>
2022-07-04 21:37:06 +0000

    medium2

--- JS_109/101_small/medium2/med2_5.js
+++ JS_109/101_small/medium2/med2_5.js
@@ -48,16 +48,59 @@ if the current integer is odd
    return the current integer.
   else move to the next iteration
 */
-function features(num) {
-    let countObj = {};
-    for ( let count = 1; count < 9876543201; count ++) {
-        let nextNum = num + count;
-        let nextNumString = String(nextNum);
-        if (!countObj.keys)
-        if (nextNum % 2 !== 1) {
-            continue;
-        } else if (nextNum % 7 !== 0) {
-            continue;
-        } else 
-    }
+function featured(num) {
+  
+   if (num >= 9876543201) {
+    return ("There is no possible number that fulfills those requirements.");
+   }
+   
+  let nextNum = 0;
+  let nextNumString  = ''
+  let uniqueNums = {}
+  
+  for ( let count = 1; count < 9876543201; count ++) {
+
+    nextNum = num + count;
+    nextNumString = String(nextNum);
+    uniqueNums = new Set(nextNumString);
+    
+      if (nextNum % 2 !== 1) {
+        continue;
+      } else if (nextNum % 7 !== 0) {
+        continue;
+      } else if (duplicates(nextNumString)) {
+        break;
+      } else {
+        continue;
+      }
+  }
+  return nextNum;
 }
+
+
+function duplicates(str) {
+  let uniqueArr = [str[0]]
+  
+  for (let count = 1; count < str.length; count++) {
+    if (!uniqueArr.includes(str[count])) {
+      uniqueArr.push(str[count])
+    }
+  }
+  if (uniqueArr.join('') === str) {
+    return true
+  } else {
+    return false;
+  }
+}
+
+
+console.log(featured(12));           // 21
+console.log(featured(20));           // 21
+console.log(featured(21));           // 35
+console.log(featured(997));          // 1029
+console.log(featured(1029));         // 1043
+console.log(featured(999999));       // 1023547
+console.log(featured(999999987));    // 1023456987
+console.log(featured(9876543186));   // 9876543201
+console.log(featured(9876543200));   // 9876543201
+console.log(featured(9876543201));   // "There is no possible number that fulfills those requirements."
