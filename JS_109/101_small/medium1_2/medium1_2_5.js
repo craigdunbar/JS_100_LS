4f79fdaab412277efb78b14d5820c064afeab4f7
Craig Dunbar <craigdunbar@hotmail.com>
2022-08-31 21:09:35 +0000

    medium again

--- JS_109/101_small/medium1_2/medium1_2_5.js
+++ JS_109/101_small/medium1_2/medium1_2_5.js
@@ -33,17 +33,25 @@ if it's not a number words then push it to the array as is
 function wordToDigit(str) {
 let wordsObj = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
 let newArr = [];
-str.split(' ').forEach(el => {
+let wordsArr = str.split(' ').map(word => {
+  if (word.endsWith('.')) {
+    return word = word.split('').slice(0, word.length -1).join('');
+  } else {
+    return word;
+  }
+})
+
+wordsArr.forEach(el => {
   if (Object.keys(wordsObj).includes(el)) {
-    console.log(el);
-    console.log(wordsObj[el])
     newArr.push(wordsObj[el]);
   } else {
     newArr.push(el)
   }
   return newArr;
 })
- return newArr.join(' '); 
+
+ let newStr = newArr.slice(0, -2).join(' ')
+ return newStr.concat('. ').concat(newArr[newArr.length - 1]).concat('.')
 }
-console.log(wordToDigit('Please call me at five five five one two three four. Thanks.'));
+console.log(wordToDigit('Please call me at five five five one two three four . Thanks.'));
 // "Please call me at 5 5 5 1 2 3 4. Thanks."
