4698668280c6de8931803f6399afe8347cf2e205
Craig Dunbar <craigdunbar@hotmail.com>
2023-02-11 07:37:28 -0800

    practice

--- codewars/6kyu/sum_of_pairs.js
+++ codewars/6kyu/sum_of_pairs.js
@@ -66,6 +66,19 @@ return the lower index pair
 // if the object contains the needed number return that number and the current element
 // if not store the current element as a key in the object
 
+// tried with an array but won't return smallest index for second
+// function sumPairs(arr, sum) {
+//   let copyArr = arr.slice(0)
+//   for (let i = 0; i < arr.length; i++) {
+//     let need = sum - arr[i];
+//     copyArr.splice(i, 1)
+//     if (copyArr.includes(need)) {
+//       return [arr[i], need]
+//     }
+//   copyArr = arr.slice(0)
+//   }
+// }
+// have to use object to build list of previusly seen numbers
 function sumPairs(arr, sum){
   let numbers = {};
   for(let i = 0; i < arr.length; i++){
@@ -82,10 +95,10 @@ function sumPairs(arr, sum){
  }
 
 console.log(sumPairs([1, 4, 8, 7, 3, 15], 8)) //, [1, 7]) //, "Basic: [1, 4, 8, 7, 3, 15] should return [1, 7] for sum = 8");
-// console.log(sumPairs([1, -2, 3, 0, -6, 1], -6)) //, [0, -6]) //, "Negatives: [1, -2, 3, 0, -6, 1] should return [0, -6] for sum = -6");
-// console.log(sumPairs([20, -13, 40], -7)) //, undefined) //, "No Match: [20, -13, 40] should return undefined for sum = -7");
+console.log(sumPairs([1, -2, 3, 0, -6, 1], -6)) //, [0, -6]) //, "Negatives: [1, -2, 3, 0, -6, 1] should return [0, -6] for sum = -6");
+console.log(sumPairs([20, -13, 40], -7)) //, undefined) //, "No Match: [20, -13, 40] should return undefined for sum = -7");
 console.log(sumPairs([1, 2, 3, 4, 1, 0], 2)) //, [1, 1]) //, "First Match From Left: [1, 2, 3, 4, 1, 0] should return [1, 1] for sum = 2");
-// console.log(sumPairs([10, 5, 2, 3, 7, 5], 10)) //, [3, 7]) //, "First Match From Left REDUX!: [10, 5, 2, 3, 7, 5] should return [3, 7] for sum = 10");
-// console.log(sumPairs([4, -2, 3, 3, 4], 8)) //, [4, 4]) //, "Duplicates: [4, -2, 3, 3, 4] should return [4, 4] for sum = 8");
-// console.log(sumPairs([0, 2, 0], 0)) //, [0, 0]) //, "Zeroes: [0, 2, 0] should return [0, 0] for sum = 0");
-// console.log(sumPairs([5, 9, 13, -3], 10)) //, [13, -3]) //, "Subtraction: [5, 9, 13, -3] should return [13, -3] for sum = 10")
+console.log(sumPairs([10, 5, 2, 3, 7, 5], 10)) //, [3, 7]) //, "First Match From Left REDUX!: [10, 5, 2, 3, 7, 5] should return [3, 7] for sum = 10");
+console.log(sumPairs([4, -2, 3, 3, 4], 8)) //, [4, 4]) //, "Duplicates: [4, -2, 3, 3, 4] should return [4, 4] for sum = 8");
+console.log(sumPairs([0, 2, 0], 0)) //, [0, 0]) //, "Zeroes: [0, 2, 0] should return [0, 0] for sum = 0");
+console.log(sumPairs([5, 9, 13, -3], 10)) //, [13, -3]) //, "Subtraction: [5, 9, 13, -3] should return [13, -3] for sum = 10")
